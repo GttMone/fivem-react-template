4035dcbfbf479fbcdb108bbbcb71478955e25ec8
GttMone <66485019+GttMone@users.noreply.github.com>
2023-03-30 18:36:46 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'React App For FiveM'
+author 'VIT0'
+version '1.0.0'
+
+
+ui_page 'html/build/index.html'
+
+client_script 'client/**/*'
+server_script 'server/**/*'
+
+files {
+	'html/build/index.html',
+	'html/build/**/*',
+}
